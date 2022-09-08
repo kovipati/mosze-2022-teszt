8026c80737b466108c1b9ef18065af829c770301
kovipati <pkovacs013@gmail.com>
2022-09-08 11:43:22 +0200

    filename change

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
+// fixing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+const int N_ELEMENTS = 100; //felesleges a constexpr, nem kell hogy ez az érték futásidőben változzon
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
+    int* b = new int[N_ELEMENTS]; //Helytelen hivatkozás, helyes: N_ELEMENTS, nincs pointer
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //cout stringet vár, helyes használat: "1-100 ertekek duplazasa", sor nincs lezárva ;-vel
+    for (int i = 0; i < N_ELEMENTS; i++) //for helyesen: for (int i = 1;i <= N_ELEMENTS; i++)
+        {
+            b[i] = (i+1) * 2; //értékadás  ==
+        }
+    for (int i = 0; i < N_ELEMENTS; i++)//for helyesen i<=100
+        {
+            std::cout << "Ertek: " << b[i] << std::endl;//helyes + << i illetve /n
+        }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;//nincs inicializálva
+    for (int i = 0; i < N_ELEMENTS; i++)//; hianyzik
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
